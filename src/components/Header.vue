b003efa0e9404bf1fe1a302c84ab7f8c0d17fc8a
nbabiera <nbabiera@newbrighton.edu.ph>
2023-09-08 19:05:28 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="Green" />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default{
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<!-- scoped means anything in here is only for this or specific component-->
+<style scoped> 
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
